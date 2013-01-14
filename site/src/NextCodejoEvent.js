e19c634afca34a57dcac4bc1cfcbedda72312667
iainjmitchell <iainjmitchell@gmail.com>
2013-01-14 20:56:16 +0000

    a commit

--- site/src/NextCodejoEvent.js
+++ site/src/NextCodejoEvent.js
@@ -1,9 +1,19 @@
 (function($, module, undefined){
 	module.NextCodejoEvent = function(context, eventDetails){
+		var titleRegExpression = new RegExp("- (.*)$");
+
 		function init(){
 			context
 				.find("#codejo-date")
-					.text(getFormattedDateTime(eventDetails.dateTime));
+					.text(getFormattedDateTime(eventDetails.dateTime))
+					.end()
+				.find("#codejo-theme")
+					.text(getEventTheme(eventDetails.title));
+		}
+
+		function getEventTheme(title){
+			var theme = title.match(titleRegExpression)[0].split("- ")[1];
+			return theme;
 		}
 
 		function getFormattedDateTime(dateTime){
--- test/tests/test-NextCodejoEvent.js
+++ test/tests/test-NextCodejoEvent.js
@@ -1,10 +1,13 @@
 (function($, undefined){
 	module("Next Codejo Event");
+	var defaultTitle = "A codejo - A theme",
+		defaultDateTime = "2000-01-01 01:00:00";
 
 	test("Codejo date displayed", function(){
 		var context = $(".codejo-details");
 		new NextCodejoEvent(context, {
-			dateTime: "2013-01-22 18:30:00" 
+			dateTime: "2013-01-22 18:30:00",
+			title: defaultTitle
 		});
 		equal(context.find("#codejo-date").text(), "Tuesday 22nd Jan (18:30)");
 	});
@@ -12,8 +15,31 @@
 	test("Another Codejo date displayed", function(){
 		var context = $(".codejo-details");
 		new NextCodejoEvent(context, {
-			dateTime: "2013-02-16 10:30:00" 
+			dateTime: "2013-02-16 10:30:00",
+			title: defaultTitle 
 		});
 		equal(context.find("#codejo-date").text(), "Saturday 16th Feb (10:30)");
+	});
+
+	test("Codejo theme is deduced from after hypen in title", function(){
+		var theme = "A theme",
+			title = "A CodeJo - " + theme,
+			context = $(".codejo-details");
+		new NextCodejoEvent(context, {
+			dateTime: "2013-02-16 10:30:00",
+			title : title
+		});
+		equal(context.find("#codejo-theme").text(), theme);
+	});
+
+	test("Another codejo theme is deduced from after hypen in title", function(){
+		var theme = "Another theme",
+			title = "A CodeJo - " + theme,
+			context = $(".codejo-details");
+		new NextCodejoEvent(context, {
+			dateTime: "2013-02-16 10:30:00",
+			title : title
+		});
+		equal(context.find("#codejo-theme").text(), theme);
 	});
 })(jQuery);
